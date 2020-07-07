a9b2a81d1b9b956d3f1754573941316c1f83980a
Anas Juwaidi <anas.didi95@gmail.com>
2020-07-07 09:50:32 +0800

    Get port number from env variable

--- src/main/java/com/anasdidi/backend/com/anasdidi/backend/MainVerticle.java
+++ src/main/java/com/anasdidi/backend/com/anasdidi/backend/MainVerticle.java
@@ -5,17 +5,24 @@ import io.vertx.core.Promise;
 
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+	@Override
+	public void start(final Promise<Void> startPromise) throws Exception {
+		int port = -1;
+		try {
+			port = Integer.parseInt(System.getProperty("server.port"));
+		} catch (Exception e) {
+			port = 8888;
+		}
+		System.out.println("HTTP server started on port " + port);
+
+		vertx.createHttpServer().requestHandler(req -> {
+			req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+		}).listen(port, http -> {
+			if (http.succeeded()) {
+				startPromise.complete();
+			} else {
+				startPromise.fail(http.cause());
+			}
+		});
+	}
 }
